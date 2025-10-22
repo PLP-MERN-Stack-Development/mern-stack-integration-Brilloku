397fb10e335fbecfcd83fcee2004cb2e24fb6b69
Brilloku <bryankuria117@gmail.com>
2025-10-22 19:16:06 +0300

    done

--- mern-blog/client/src/App.jsx
+++ mern-blog/client/src/App.jsx
@@ -15,7 +15,7 @@ import './App.css';
 function App() {
   return (
     <AuthProvider>
-      <Router>
+      <Router future={{ v7_startTransition: true, v7_relativeSplatPath: true }}>
         <div className="App">
           <Navbar />
           <main className="main-content">
